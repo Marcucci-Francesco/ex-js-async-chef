400d39b485b9cf90da39b990103eee6adde494db
Marcucci-Francesco <fmarcucci47@gmail.com>
2025-04-04 16:19:34 +0200

    [add] recipe birthdate chef

---
+++ script.js
@@ -0,0 +1,14 @@
+const getChefBirthday = async (id) => {
+  const responseUser = await fetch(`https://dummyjson.com/recipes/${id}`)
+  const recipeDate = await responseUser.json();
+  const userId = recipeDate.userId;
+  const responseChef = await fetch(`https://dummyjson.com/users/${userId}`)
+  const recipeChef = await responseChef.json();
+  const birthdayChef = recipeChef.birthDate;
+  return birthdayChef;
+}
+
+(async () => {
+  const birthdayChef = await getChefBirthday(1);
+  console.log('Data di nascita dello Chef:', birthdayChef);
+})();
